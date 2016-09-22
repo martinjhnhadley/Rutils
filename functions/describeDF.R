51fe519d58d33b363d35eef5355a1e92d24f3e7f
davidruvolo51 <davidruvolo51@gmail.com>
2016-09-22 02:24:33 +0100

    DescribeDF: Update HEADER

--- functions/describeDF.R
+++ functions/describeDF.R
@@ -1,13 +1,19 @@
-## ################################
-## File: describeDF.R
-## Author: David Ruvolo
-## Created: 18 August 2016
-## Purpose: describe DF
-## Main Call: describeDF(dataframe)
-## ################################
-
+#' =========================================================
+#' File: describeDF.R
+#' Author: David Ruvolo
+#' Created: 18 August 2016
+#' Modified: 22 September 2016
+#' Purpose: quick and dirty way to describe DF
+#' Use: source file and then call function
+#' Notes: NA
+#' =========================================================
+#' BEGIN FUNCTIONS
 describeDF <- function(dataframe){
+    
+    #' essentially builds DF output inline
     data.frame(
+        
+        #' col 1: Measure
         "Measure" = c(
                     "Columns",                  ## number of columns
                     "Rows",                     ## number of rows
@@ -16,6 +22,8 @@ describeDF <- function(dataframe){
                     "Num Cols Numeric",         ## cols whose class = numeric
                     "Num Cols Integer"          ## cols whose class = integer
                      ),
+        
+        #' col 2: count
         "Count" = c(
                     NCOL(dataframe),                                              ## cols count
                     NROW(dataframe),                                              ## row count
